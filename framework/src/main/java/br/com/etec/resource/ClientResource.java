d559092c7d38056bbd441cb724a1883c9b507492
MrSpock182 <klebernunes182@gmail.com>
2025-03-25 02:15:25 -0300

    Atualizando ECS

--- framework/src/main/java/br/com/etec/resource/ClientResource.java
+++ framework/src/main/java/br/com/etec/resource/ClientResource.java
@@ -8,7 +8,6 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 public class ClientResource {
 
-
     @ResponseStatus(OK)
     @GetMapping("/healthcheck")
     public String healthcheck() {
@@ -19,6 +18,12 @@ public class ClientResource {
     @PostMapping("/user")
     public String nameUser(@RequestBody final UserRequest request) {
         return request.name();
+    }
+
+    @ResponseStatus(OK)
+    @GetMapping("/update")
+    public String update() {
+        return "RECURSO ATUALIZADO";
     }
 
 }
