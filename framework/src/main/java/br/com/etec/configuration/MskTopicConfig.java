0d118a559dcc9093426595a8d39e08197af97c53
MrSpock182 <klebernunes182@gmail.com>
2025-03-31 14:48:17 -0300

    teste de criacao de topico

---
+++ framework/src/main/java/br/com/etec/configuration/MskTopicConfig.java
@@ -0,0 +1,31 @@
+package br.com.etec.configuration;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class MskTopicConfig {
+    @Bean
+    public NewTopic mksTopic(@Value("${spring.kafka.topic.msk-topic}") final String topic) {
+        return TopicBuilder
+                .name(topic)
+                .partitions(10)
+                .replicas(2)
+                .compact()
+                .configs(topicConfigs())
+                .build();
+    }
+
+    private Map<String, String> topicConfigs() {
+        String time = "86500000";
+        Map<String, String> map = new HashMap<>();
+        map.put("delete.retention.ms", time);
+        return map;
+    }
+}
--- framework/src/main/java/br/com/etec/messaging/UserProducerWithKafka.java
+++ framework/src/main/java/br/com/etec/messaging/UserProducerWithKafka.java
@@ -3,29 +3,35 @@ package br.com.etec.messaging;
 import br.com.etec.messaging.dto.UserProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Component
 public class UserProducerWithKafka {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserProducerWithKafka.class);
 
+    private final String topic;
     private final KafkaTemplate<String, UserProducer> kafkaTemplate;
 
-    public UserProducerWithKafka(KafkaTemplate<String, UserProducer> kafkaTemplate) {
+    public UserProducerWithKafka(
+            KafkaTemplate<String, UserProducer> kafkaTemplate,
+            @Value("${spring.kafka.topic.msk-topic}") String topic) {
+        this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void produce(final String name) {
-        kafkaTemplate.send("mks-topic", UUID.randomUUID().toString(), new UserProducer(name));
-
-//        try {
-//            kafkaTemplate.send("mks-topic", UUID.randomUUID().toString(), new UserProducer(name))
-//                    .get(5, TimeUnit.SECONDS);
-//        } catch (InterruptedException | ExecutionException | TimeoutException ex) {
-//            LOGGER.error("Producing error {}", name, ex);
-//        }
+        try {
+            kafkaTemplate.send(topic, UUID.randomUUID().toString(), new UserProducer(name))
+                    .get(5, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException ex) {
+            LOGGER.error("Producing error {}", name, ex);
+        }
     }
 }
